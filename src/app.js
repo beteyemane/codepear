03ae5335c827b622170645dd952b87659246d040
byemane <beteyemane@hotmail.co.uk>
2019-04-10 13:19:59 +0100

    front end set up with react

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('hi')
-})
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style.scss'
+
+class App extends React.Component{
+  constructor() {
+    super()
+
+
+  }
+
+  render(){
+    return(
+      <h1>Codepear</h1>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+)
